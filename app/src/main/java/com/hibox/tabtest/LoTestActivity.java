a215d3d9159d999362358a986a0a2f59d3ae4ef9
Robert.Hao <robert.hao@dudu.tm>
2018-06-01 01:34:00 +0800

    测试

---
+++ app/src/main/java/com/hibox/tabtest/LoTestActivity.java
@@ -0,0 +1,60 @@
+package com.hibox.tabtest;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class LoTestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        showStatusBar();
+        setContentView(R.layout.activity_lo_test);
+    }
+
+    private void showStatusBar() { //使得布局内容可以填充整个屏幕，状态栏悬浮在布局上面，
+//        WindowManager.LayoutParams attrs = getWindow().getAttributes();
+//        attrs.flags &= ~WindowManager.LayoutParams.FLAG_FULLSCREEN;
+//        getWindow().setAttributes(attrs);
+//
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+
+
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+//        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+
+
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+//            WindowManager.LayoutParams flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+//            if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+//                //将侧边栏顶部延伸至status bar
+//                mDrawerLayout.setFitsSystemWindows(true);
+//                //将主页面顶部延伸至status bar;虽默认为false,但经测试,DrawerLayout需显示设置
+//                mDrawerLayout.setClipToPadding(false);
+//            }
+//        }
+
+        init();
+
+    }
+
+    private void init(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/hibox/tabtest/MainActivity.java
@@ -0,0 +1,166 @@
+package com.hibox.tabtest;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import devlight.io.library.ntb.NavigationTabBar;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initUI();
+    }
+
+    private void initUI() {
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.vp_vertical_ntb);
+        viewPager.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return 8;
+            }
+
+            @Override
+            public boolean isViewFromObject(final View view, final Object object) {
+                return view.equals(object);
+            }
+
+            @Override
+            public void destroyItem(final View container, final int position, final Object object) {
+                ((ViewPager) container).removeView((View) object);
+            }
+
+            @Override
+            public Object instantiateItem(final ViewGroup container, final int position) {
+                final View view = LayoutInflater.from(
+                        getBaseContext()).inflate(R.layout.item_vp, null, false);
+
+                final TextView txtPage = (TextView) view.findViewById(R.id.txt_vp_item_page);
+                txtPage.setText(String.format("Page #%d", position));
+
+                container.addView(view);
+                return view;
+            }
+        });
+
+        final String[] colors = getResources().getStringArray(R.array.vertical_ntb);
+
+        final NavigationTabBar navigationTabBar = (NavigationTabBar) findViewById(R.id.ntb_vertical);
+        final ArrayList<NavigationTabBar.Model> models = new ArrayList<>();
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_first),   // 默认icon
+                        Color.parseColor(colors[0])) //选中时的背景色
+//                        .title("ic_first") //tab 的 title
+//                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth)) //选中时的icon
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_second),
+                        Color.parseColor(colors[1]))
+//                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
+//                        .title("ic_second")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_third),
+                        Color.parseColor(colors[2]))
+//                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
+//                        .title("ic_third")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_fourth),
+                        Color.parseColor(colors[3]))
+//                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
+//                        .title("ic_fourth")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_fifth),
+                        Color.parseColor(colors[4]))
+                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
+                        .title("ic_fifth")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_sixth),
+                        Color.parseColor(colors[5]))
+                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
+                        .title("ic_sixth")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_seventh),
+                        Color.parseColor(colors[6]))
+                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
+                        .title("ic_seventh")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_eighth),
+                        Color.parseColor(colors[7]))
+                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
+                        .title("ic_eighth")
+                        .build()
+        );
+
+        navigationTabBar.setModels(models);
+        navigationTabBar.setBadgeBgColor(Color.RED); //勋章相关 ，就是跟qq消息那种小红点类似
+        navigationTabBar.setBgColor(Color.YELLOW); // 设置tab 默认背景色
+        navigationTabBar.setIconSizeFraction(1.0f); //图标所占tab大小比例
+        navigationTabBar.setIsScaled(false); //设置是否进行icon点击缩放
+        navigationTabBar.setIsSwiped(false);
+        navigationTabBar.setIsTitled(false);
+        navigationTabBar.setViewPager(viewPager, 4);
+
+        navigationTabBar.setOnTabBarSelectedIndexListener(new NavigationTabBar.OnTabBarSelectedIndexListener() {
+            @Override
+            public void onStartTabSelected(NavigationTabBar.Model model, int index) {
+
+            }
+
+            @Override
+            public void onEndTabSelected(NavigationTabBar.Model model, int index) {
+                Toast.makeText(MainActivity.this, "切换： "+index,Toast.LENGTH_LONG).show();
+            }
+        });
+
+        navigationTabBar.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//                Toast.makeText(MainActivity.this, "滚动： ",Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Toast.makeText(MainActivity.this, "选择： ",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+}
